fcc13b56efdea9ef4c1b9c7c25f22b8ca54bca12
unknown <klavs.sprugevics@gmail.com>
2021-04-18 15:51:06 +0300

    Added related posts to single page view.

--- news_list/views.py
+++ news_list/views.py
@@ -7,7 +7,7 @@ from .models import News
 def home(request):
 
     # No musu sql datubazes izpildam query, kas ekvivalents - SELECT * FROM POST;
-    data = News.objects.all()
+    data = News.objects.all().order_by('-date')[:5]
 
     # Redirecto lietotaju uz lapu un klat iedod http req datus
     return render(request, "index.html", {"posts":data})
@@ -17,4 +17,8 @@ def post(request, slug):
     # No musu sql datubazes izpilda m query, kas ekvivalents - SELECT * FROM POST WHERE slug = this.slug;
     data = News.objects.get(slug=slug)
 
-    return render(request, 'postview.html', {'post':data})
+    # Papildus panemam no datubas 3. jaunakos postus saja kategorija, lai pievienotu kaa ieteicamos
+    related_posts = News.objects.filter(category=data.category).order_by('-date').exclude(slug=slug)[:3]
+
+    
+    return render(request, 'postview.html', {'post':data, 'related':related_posts})
